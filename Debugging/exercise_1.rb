c64a728cca60ede33a33462720d225e3677e6e8c
Mergim <mergimberisha0@gmail.com>
2023-04-13 15:45:40 +0100

    debugging exercise 1

---
+++ Debugging/exercise_1.rb
@@ -0,0 +1,14 @@
+# Exercise 1 challenge
+# def say_hello(name)
+#     return "hello #(name)"
+#   end
+
+# Intended output:
+# > say_hello("kay")
+# => "hello kay"
+
+def say_hello(name)
+  "hello #{name}"
+end
+
+puts say_hello("kay")
