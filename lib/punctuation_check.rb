401b23ab94e8030b561735dfbe83e18493f030e9
Cyryl Gotkowicz <cyrylgotkowicz@gmail.com>
2023-04-13 14:39:41 +0100

    Changed variable names

--- lib/punctuation_check.rb
+++ lib/punctuation_check.rb
@@ -13,10 +13,10 @@ class Punctuation
     def capital_letter
         first_letter_cap = @text[0].upcase
         if first_letter_cap == @text[0]
-            @x = true
+            @x = "cap"
             true
         else
-            @x = false
+            @x = "not_cap"
             false
         end
     end
@@ -24,18 +24,18 @@ class Punctuation
     def punctuation
         last_character = @text[-1]
         if last_character == "." || last_character == "!" || last_character == "?"
-            @y = true
+            @y = "punctuation"
             true
         else
-            @y = false
+            @y = "no_punctuation"
             false
         end
     end
 
     def print
-        if @x == true && @y == false
+        if @x == "cap" && @y == "no_punctuation"
             "Text has capital letter but no punctuation"
-        elsif @x == false && @y == true
+        elsif @x == "not_cap" && @y == "punctuation"
             "Text has no capital letter but has punctuation"
         end
     end
